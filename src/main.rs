cce5a6a12480e94e99c6060295eb478ca134a576
Dennis Sonntag <sonntagdennis8@gmail.com>
2023-05-21 23:20:43 -0600

    feat: movement and sounds
    Movement now works including turns and taking although legal moves are not yet
    implemented. highlighting for selected piece and moved piece. Sound effects for
    taking and moving.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@
 
 use std::collections::HashMap;
 
-use bevy::{
-	prelude::*,
-	window::{ExitCondition, PresentMode},
-};
+use bevy::{prelude::*, window::PresentMode};
 
 const WINDOW_SIZE: f32 = 600.;
 
@@ -15,6 +12,16 @@ const SQUARE_SIZE: f32 = WINDOW_SIZE / 8.;
 struct BoardResource {
 	board: [Piece; 64],
 }
+
+struct TakeEvent;
+
+struct MoveEvent {
+	row: Option<u8>,
+	col: Option<u8>,
+}
+
+#[derive(Debug, Clone, Copy, Component, PartialEq)]
+struct MovedSquare;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Pieces {
@@ -27,6 +34,9 @@ enum Pieces {
 	Pawn,
 }
 
+#[derive(Debug, Clone, Copy, Component, PartialEq)]
+struct HighlightSquare;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum PieceColor {
 	White,
@@ -35,7 +45,7 @@ enum PieceColor {
 }
 
 #[derive(Debug, Clone, Copy, Component, PartialEq)]
-pub struct Piece {
+struct Piece {
 	row: Option<u8>,
 	col: Option<u8>,
 	piece: Pieces,
@@ -50,6 +60,22 @@ impl Default for Piece {
 			color: PieceColor::None,
 			piece: Pieces::None,
 		}
+	}
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Default, States)]
+enum Turn {
+	#[default]
+	White,
+	Black,
+}
+
+impl PartialEq<Turn> for PieceColor {
+	fn eq(&self, other: &Turn) -> bool {
+		matches!(
+			(self, other),
+			(PieceColor::White, Turn::White) | (PieceColor::Black, Turn::Black)
+		)
 	}
 }
 
@@ -150,10 +176,17 @@ fn main() {
 		.insert_resource(Msaa::Sample8)
 		.init_resource::<BoardResource>()
 		.init_resource::<SelectedPiece>()
+		.add_state::<Turn>()
+		.add_event::<MoveEvent>()
+		.add_event::<TakeEvent>()
 		.add_startup_system(setup_camera)
 		.add_startup_system(spawn_board)
 		.add_startup_system(first_draw_board)
 		.add_system(move_piece_system)
+		.add_system(highlight_selected_system)
+		.add_system(highlight_moved_system)
+		.add_system(play_move_sound_system)
+		.add_system(play_take_sound_system)
 		.run();
 }
 
@@ -220,7 +253,7 @@ fn first_draw_board(
 						translation: Vec3::new(
 							col as f32 * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.),
 							row as f32 * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.),
-							0.0,
+							2.0,
 						),
 						scale: Vec3::splat(WINDOW_SIZE / 2500.),
 						..default()
@@ -236,6 +269,44 @@ fn first_draw_board(
 				});
 		}
 	}
+
+	commands
+		.spawn((SpriteBundle {
+			transform: Transform {
+				translation: Vec3::new(
+					-(WINDOW_SIZE / 2.) + ((0. - 0.5) * SQUARE_SIZE),
+					-(WINDOW_SIZE / 2.) + ((0. - 0.5) * SQUARE_SIZE),
+					1.0,
+				),
+				scale: Vec3::new(SQUARE_SIZE, SQUARE_SIZE, 0.0),
+				..default()
+			},
+			sprite: Sprite {
+				color: Color::rgba_u8(255, 255, 0, 100),
+				..default()
+			},
+			..default()
+		},))
+		.insert(HighlightSquare);
+
+	commands
+		.spawn((SpriteBundle {
+			transform: Transform {
+				translation: Vec3::new(
+					-(WINDOW_SIZE / 2.) + ((0. - 0.5) * SQUARE_SIZE),
+					-(WINDOW_SIZE / 2.) + ((0. - 0.5) * SQUARE_SIZE),
+					1.0,
+				),
+				scale: Vec3::new(SQUARE_SIZE, SQUARE_SIZE, 0.0),
+				..default()
+			},
+			sprite: Sprite {
+				color: Color::rgba_u8(200, 115, 0, 100),
+				..default()
+			},
+			..default()
+		},))
+		.insert(MovedSquare);
 }
 
 fn move_piece_system(
@@ -243,7 +314,12 @@ fn move_piece_system(
 	windows: Query<&Window>,
 	mut board: ResMut<BoardResource>,
 	mut selected_piece: ResMut<SelectedPiece>,
-	mut pieces: Query<(&Piece, &mut Transform)>,
+	mut pieces: Query<(&mut Piece, &mut Transform, Entity)>,
+	mut next_state: ResMut<NextState<Turn>>,
+	current_state: Res<State<Turn>>,
+	mut commands: Commands,
+	mut ev_move: EventWriter<MoveEvent>,
+	mut ev_take: EventWriter<TakeEvent>,
 ) {
 	let window = windows.get_single().unwrap();
 
@@ -256,55 +332,151 @@ fn move_piece_system(
 
 			let clicked_piece = board.board[index];
 
-			// board.board.iter().for_each(|x| println!("{:?}", x));
-			if selected_piece.0.is_none() && clicked_piece.piece != Pieces::None {
+			if Some(clicked_piece) == selected_piece.0 && selected_piece.0.is_some() {
+				selected_piece.0 = None;
+				ev_move.send(MoveEvent {
+					row: None,
+					col: None,
+				});
+			} else if clicked_piece.piece != Pieces::None && clicked_piece.color == current_state.0
+			{
 				selected_piece.0 = Some(clicked_piece);
+				ev_move.send(MoveEvent {
+					row: None,
+					col: None,
+				});
 			};
 
-			if selected_piece.0 != Some(clicked_piece) && selected_piece.0.is_some() {
-				for (piece, mut transform) in pieces.iter_mut() {
+			if selected_piece.0 != Some(clicked_piece)
+				&& selected_piece.0.is_some()
+				&& clicked_piece.color != current_state.0
+			{
+				for (mut piece, mut transform, entity) in pieces.iter_mut() {
 					if Some(*piece) == selected_piece.0 {
 						transform.translation.x =
 							col as f32 * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.);
 						transform.translation.y =
 							row as f32 * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.);
+						piece.row = Some(row);
+						piece.col = Some(col);
+
+						let old_index =
+							(piece.row.unwrap_or(0) * 8 + piece.col.unwrap_or(0)) as usize;
+						board.board[old_index] = Piece {
+							..Default::default()
+						};
+
+						board.board[index] = *piece;
+						board.board[index].row = Some(row);
+						board.board[index].col = Some(col);
+
+						ev_move.send(MoveEvent {
+							row: Some(row),
+							col: Some(col),
+						});
+					}
+					if *piece.as_ref() == clicked_piece
+						&& clicked_piece.color != PieceColor::None
+						&& clicked_piece.color != current_state.0
+					{
+						commands.entity(entity).despawn_recursive();
+						ev_take.send(TakeEvent);
 					}
 				}
 
-				if let Some(piece) = selected_piece.0 {
-					let old_index = (piece.row.unwrap_or(0) * 8 + piece.col.unwrap_or(0)) as usize;
-					board.board[old_index] = Piece {
-						..Default::default()
-					};
-					board.board[index] = piece;
-					board.board[index].row = Some(row);
-					board.board[index].col = Some(col);
-				}
-
 				selected_piece.0 = None;
+				next_state.set(match current_state.0 {
+					Turn::White => Turn::Black,
+					Turn::Black => Turn::White,
+				});
 			}
 		}
 	}
 }
 
-fn print_board(board: [Piece; 64]) {
-	for row in (0..8).rev() {
-		print!("|");
-		for col in 0..8 {
-			let index = (row * 8 + col) as usize;
-			match board[index].piece {
-				Pieces::None => print!(" "),
-				piece => print!(
-					"{}",
-					format!("{:?}", piece)
-						.chars()
-						.next()
-						.unwrap()
-						.to_lowercase()
-				),
-			}
-			print!("|");
-		}
-		println!();
+fn highlight_selected_system(
+	selected: Res<SelectedPiece>,
+	mut highlight_square: Query<(&HighlightSquare, &mut Transform)>,
+) {
+	let mut highlight_square = highlight_square.get_single_mut().unwrap();
+	if let Some(selected) = selected.0 {
+		highlight_square.1.translation.x = selected.col.unwrap_or(0) as f32 * SQUARE_SIZE
+			- (WINDOW_SIZE / 2.)
+			+ (SQUARE_SIZE / 2.);
+		highlight_square.1.translation.y = selected.row.unwrap_or(0) as f32 * SQUARE_SIZE
+			- (WINDOW_SIZE / 2.)
+			+ (SQUARE_SIZE / 2.);
+	} else {
+		highlight_square.1.translation.x =
+			-1. * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.);
+		highlight_square.1.translation.y =
+			-1. * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.);
 	}
 }
+
+fn highlight_moved_system(
+	mut move_square: Query<(&MovedSquare, &mut Transform)>,
+	mut ev_move: EventReader<MoveEvent>,
+) {
+	let mut moved_square = move_square.get_single_mut().unwrap();
+	for i in ev_move.iter() {
+		if let (Some(row), Some(col)) = (i.row, i.col) {
+			moved_square.1.translation.x =
+				col as f32 * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.);
+			moved_square.1.translation.y =
+				row as f32 * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.);
+		} else {
+			moved_square.1.translation.x =
+				-1. * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.);
+			moved_square.1.translation.y =
+				-1. * SQUARE_SIZE - (WINDOW_SIZE / 2.) + (SQUARE_SIZE / 2.);
+		}
+	}
+}
+
+fn play_move_sound_system(
+	mut ev_move: EventReader<MoveEvent>,
+	mut ev_take: EventReader<TakeEvent>,
+	asset_server: Res<AssetServer>,
+	audio: Res<Audio>,
+) {
+	for i in ev_move.iter() {
+		if i.row.is_some() && ev_take.iter().count() == 0 {
+			let music = asset_server.load("sounds/move.ogg");
+			audio.play(music);
+		}
+	}
+}
+
+fn play_take_sound_system(
+	mut ev_take: EventReader<TakeEvent>,
+	asset_server: Res<AssetServer>,
+	audio: Res<Audio>,
+) {
+	if ev_take.iter().count() > 0 {
+		let music = asset_server.load("sounds/take.ogg");
+		audio.play(music);
+	}
+}
+
+// fn print_board(board: [Piece; 64]) {
+// 	for row in (0..8).rev() {
+// 		print!("|");
+// 		for col in 0..8 {
+// 			let index = (row * 8 + col) as usize;
+// 			match board[index].piece {
+// 				Pieces::None => print!(" "),
+// 				piece => print!(
+// 					"{}",
+// 					format!("{:?}", piece)
+// 						.chars()
+// 						.next()
+// 						.unwrap()
+// 						.to_lowercase()
+// 				),
+// 			print!("|");
+// 			}
+// 		}
+// 		println!();
+// 	}
+// }
